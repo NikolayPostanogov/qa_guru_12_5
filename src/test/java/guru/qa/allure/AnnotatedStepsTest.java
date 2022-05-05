7db246bd86afdf2c311678ec934ba530f31cf049
NikolayPostanogov <postanogov@nwtn-group.com>
2022-05-06 02:27:18 +0500

    add Allure add Selenide Test (with Listener) add LambdaSteps Test add AnnotatedSteps Test

---
+++ src/test/java/guru/qa/allure/AnnotatedStepsTest.java
@@ -0,0 +1,21 @@
+package guru.qa.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedStepsTest {
+    private static final String REPOSITORY = "NikolayPostanogov/qa_guru_12_5";
+    private static final int ISSUE_NUMBER = 1;
+    @Test
+    public void testGitHubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE_NUMBER);
+    }
+}
---
+++ src/test/java/guru/qa/allure/SelenideTest.java
@@ -0,0 +1,28 @@
+package guru.qa.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+    @Test
+    public void testGitHubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("NikolayPostanogov/qa_guru_12_5");
+        $(".header-search-input").submit();
+
+        $(linkText("NikolayPostanogov/qa_guru_12_5")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#1")).should(Condition.visible);
+    }
+}
